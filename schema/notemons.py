dae30c267f90a9f216dbedb7f2307138023bd277
Lukas Himbert <53428415+hwjsnc@users.noreply.github.com>
2021-05-22 05:02:22 +0200

    Add schema for monster notebook (t_notemons.tbl) (#1)

---
+++ schema/notemons.py
@@ -0,0 +1,16 @@
+QSChapter = {
+    "section_id" : "s",
+    "section_name" : "t",
+    "short_1" : "s",
+}
+
+QSMons = {
+    "enemy_script": "t",
+    "string_1": "t",
+    "section_id": "s",
+    "short_1": "s",
+    "is_extra": "b",
+    "byte_1": "b",
+}
+
+headers = ["QSChapter", "QSMons"]
