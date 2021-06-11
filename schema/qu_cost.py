32de117ae61bd3a8a8927b3d158973aaf696c95f
Lukas Himbert <53428415+hwjsnc@users.noreply.github.com>
2021-06-11 16:20:00 +0200

    schema for t_qu_cost.tbl (quartz synthesis cost) (#4)

---
+++ schema/qu_cost.py
@@ -0,0 +1,12 @@
+QuartzCost = {
+    "quartz_id" : "s",
+    "earth_cost" : "s",
+    "water_cost" : "s",
+    "fire_cost" : "s",
+    "wind_cost" : "s",
+    "time_cost" : "s",
+    "space_cost" : "s",
+    "mirage_cost" : "s",
+}
+
+headers = ['QuartzCost']
