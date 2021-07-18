6855a156e455a3bbd8a2fbb40654f6a960140c97
Nhan Nguyen <Nhan.Nguyen@unb.ca>
2021-07-17 22:12:09 -0300

    Add schema for t_attach and t_name

---
+++ schema/attach.py
@@ -0,0 +1,9 @@
+AttachTableData = {
+    "character_id" : "s",
+    "type" : "s",
+    "unknown18" : "d18",
+    "model" : "t",
+    "attach_point": "t"
+}
+
+headers = ['AttachTableData']
---
+++ schema/name.py
@@ -0,0 +1,11 @@
+NameTableData = {
+    "character_id" : "s",
+    "character_name" : "t",
+    "model" : "t",
+    "animation" : "t",
+    "face_default" : "t",
+    "face" : "t",
+    "data" : "d"
+}
+
+headers = ['NameTableData']
